b6d3e32aa709f603e069ee2fb9dda60a426e4292
Vadim Yakhin <yakhin.v@gmail.com>
2019-06-21 13:03:39 -0300

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <SiteHeader />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SiteHeader from "./components/SiteHeader.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    SiteHeader
   }
 };
 </script>
@@ -23,6 +23,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="hello">
+    <h1>Vue search-ui demo (with Hearthstone cards)</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SiteHeader"
+};
+</script>
+
+<style scoped></style>
