adb8b97d3cde7db10c2a37789119835ff9113254
Vadim Yakhin <yakhin.v@gmail.com>
2019-06-21 16:09:25 -0300

    fetch data on submit and show results

--- src/components/SearchSection.vue
+++ src/components/SearchSection.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>SearchSection</div>
+  <div>
+    <div>SearchSection</div>
+    <div>
+      <form action="" @submit.prevent="handleFormSubmit">
+        <input type="text" v-model="searchInputValue" />
+      </form>
+      <ul>
+        <li v-for="result in searchState.results" :key="result.card_id.raw">
+          <img
+            :src="result.img.raw"
+            alt=""
+            v-if="result.img && result.img.raw"
+          />
+          {{ JSON.stringify(result) }}
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -18,10 +35,26 @@ const config = {
 
 const driver = new SearchDriver(config);
 
-driver.subscribeToStateChanges(state =>
-  console.log(`Received ${state.totalResults} results for your search!`)
-);
-
-driver.getActions().setSearchTerm("canyon");
-export default {};
+export default {
+  data() {
+    return {
+      searchInputValue: "",
+      searchState: {
+        results: []
+      }
+    };
+  },
+  computed: {},
+  methods: {
+    handleFormSubmit() {
+      driver.getActions().setSearchTerm(this.searchInputValue);
+    }
+  },
+  mounted() {
+    driver.subscribeToStateChanges(state => {
+      console.log(`Received ${state.totalResults} results for your search!`);
+      this.searchState = state;
+    });
+  }
+};
 </script>
