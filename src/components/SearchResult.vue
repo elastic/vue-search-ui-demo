3d31f8ca3cb22e56650894a3ac932159e7c71b36
Vadim Yakhin <yakhin.v@gmail.com>
2019-06-24 13:30:39 -0300

    add more fields to SearchResult

--- src/components/SearchResult.vue
+++ src/components/SearchResult.vue
@@ -7,19 +7,32 @@
     />
     <div>
       <div>Name: {{ result.name.raw }}</div>
-      <div>Flavor: {{ result.flavor.raw }}</div>
+      <div v-if="result.flavor">Flavor: {{ result.flavor.raw }}</div>
       <br />
-      <div>Class: {{ result.card_class.raw }}</div>
-      <div>Type: {{ result.type.raw }}</div>
+      <div v-if="result.classes">
+        Classes: {{ JSON.stringify(result.classes.raw) }}
+      </div>
+      <div v-else-if="result.card_class">
+        Class: {{ result.card_class.raw }}
+      </div>
+      <div v-if="result.type">Type: {{ result.type.raw }}</div>
       <div v-if="result.race">Race: {{ result.race.raw }}</div>
       <div>Set: {{ result.set.raw }}</div>
       <div>Rarity: {{ result.rarity.raw }}</div>
       <br />
-      <div>Cost: {{ result.cost.raw }}</div>
+      <div v-if="result.cost">Cost: {{ result.cost.raw }}</div>
       <div v-if="result.attack">Attack: {{ result.attack.raw }}</div>
+      <div v-if="result.durability">
+        Durability: {{ result.durability.raw }}
+      </div>
       <div v-if="result.health">Health: {{ result.health.raw }}</div>
+      <div v-if="result.armor">Armor: {{ result.armor.raw }}</div>
+      <div v-if="result.spell_damage">
+        Spell damage: {{ result.spell_damage.raw }}
+      </div>
+      <div v-if="result.overload">Overload: {{ result.overload.raw }}</div>
       <br />
-      <div>Artist: {{ result.artist.raw }}</div>
+      <div v-if="result.artist">Artist: {{ result.artist.raw }}</div>
     </div>
   </div>
 </template>
